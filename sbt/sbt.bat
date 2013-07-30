b782a88eb41670ea6ff088b032b8f05992921b5c
Ngoc Dao <ngocdaothanh@gmail.com>
2013-07-30 21:43:23 +0900

    Fix sbt.bat

--- sbt/sbt.bat
+++ sbt/sbt.bat
@@ -1,2 +1,2 @@
-$ set SCRIPT_DIR=%~dp0
-$ java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%sbt-launch-0.12.4.jar" %*
+set SCRIPT_DIR=%~dp0
+java -Xms256M -Xmx512M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%sbt-launch-0.12.4.jar" %*
