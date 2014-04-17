ca3c14adee52b255d5602d7b638f4b41b7b33751
Ngoc Dao <ngocdaothanh@gmail.com>
2014-04-17 15:46:32 +0900

    Update SBT from 0.13.1 to 0.13.2

--- sbt/sbt.bat
+++ sbt/sbt.bat
@@ -1,2 +1,2 @@
 set SCRIPT_DIR=%~dp0
-java -Xms256M -Xmx512M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%sbt-launch-0.13.1.jar" %*
+java -Xms256M -Xmx512M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%sbt-launch-0.13.2.jar" %*
