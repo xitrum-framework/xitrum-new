cf3280f9e7be663dce23c931b6fd300454ae283f
Ngoc Dao <ngocdaothanh@gmail.com>
2014-10-16 15:27:42 +0900

    To display time when the application is stopped for GC: -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime

--- script/runner.bat
+++ script/runner.bat
@@ -1,4 +1,6 @@
-# You may need to customize memory config below to optimize for your environment
+# You may need to customize memory config below to optimize for your environment.
+# To display time when the application is stopped for GC:
+# -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime
 set JAVA_OPTS=-Xmx1024m -Xms256m -XX:MaxPermSize=128m -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1 -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -Djava.awt.headless=true -server -Dxitrum.mode=production
 
 set ROOT_DIR=%~dp0..
