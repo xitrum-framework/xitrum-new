ccad129f26706e816602c5edd194367c60e515b2
Ngoc Dao <ngocdaothanh@gmail.com>
2018-07-26 11:57:57 +1000

    Decrease default max memory usage from 1GB to 512MB (Amazon tiny instance only has 1GB)

--- script/runner.bat
+++ script/runner.bat
@@ -3,7 +3,7 @@
 rem You may need to customize memory config below to optimize for your environment.
 rem To display time when the application is stopped for GC:
 rem -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime
-set JAVA_OPTS=-Xmx1024m -Xms256m -XX:MaxMetaspaceSize=384m -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1 -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -Djava.awt.headless=true -server -Dxitrum.mode=production
+set JAVA_OPTS=-Xmx512m -Xms256m -XX:MaxMetaspaceSize=384m -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1 -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -Djava.awt.headless=true -server -Dxitrum.mode=production
 
 set ROOT_DIR=%~dp0..
 cd "%$ROOT_DIR%"
