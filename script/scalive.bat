7d743eb8b7c76703fb52e18e9f76372663295423
Ngoc Dao <ngocdaothanh@gmail.com>
2019-01-31 00:48:25 +1100

    Update Scalive from 1.6 to 1.7.0

--- script/scalive.bat
+++ script/scalive.bat
@@ -3,6 +3,6 @@ set JAVA_OPTS=-Djava.awt.headless=true
 set ROOT_DIR=%~dp0..
 cd "%$ROOT_DIR%"
 
-set CLASS_PATH="%ROOT_DIR%\script\*;."
+set CLASS_PATH="%ROOT_DIR%\script\*;%ROOT_DIR%\lib\*"
 
 java %JAVA_OPTS% -cp %CLASS_PATH% scalive.client.AgentLoader "%$ROOT_DIR%\script;%$ROOT_DIR%\lib" %*
